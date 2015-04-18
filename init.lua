cdf7036ca1ce7a23370ca5f7a30802d09ea3be08
Loic Blot <loic.blot@unix-experience.fr>
2015-04-18 10:11:22 +0000

    Optimize performance by fetching objects_inside_radius only one time (it's an heavy call) and testing the distance * Remove the two loops, iterate only in one unique loop * Test the distance, if <= 1m or not

--- init.lua
+++ init.lua
@@ -5,63 +5,64 @@ minetest.register_globalstep(function(dtime)
 			pos.y = pos.y+0.5
 			local inv = player:get_inventory()
 			
-			for _,object in ipairs(minetest.env:get_objects_inside_radius(pos, 1)) do
+			local objectlist = minetest.env:get_objects_inside_radius(pos, 2)
+			for _,object in ipairs(objectlist) do
 				if not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "__builtin:item" then
-					if inv and inv:room_for_item("main", ItemStack(object:get_luaentity().itemstring)) then
-						inv:add_item("main", ItemStack(object:get_luaentity().itemstring))
-						if object:get_luaentity().itemstring ~= "" then
-							minetest.sound_play("item_drop_pickup", {
-								to_player = player:get_player_name(),
-								gain = 0.4,
-							})
-						end
-						object:get_luaentity().itemstring = ""
-						object:remove()
-					end
-				end
-			end
-			
-			for _,object in ipairs(minetest.env:get_objects_inside_radius(pos, 2)) do
-				if not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "__builtin:item" then
-					if object:get_luaentity().collect then
+					local pos2 = object:getpos()
+					local distance = math.sqrt(((pos2.x - pos.x) * (pos2.x - pos.x)) + ((pos2.y - pos.y) * (pos2.y - pos.y))
+						+ ((pos2.z - pos.z) * (pos2.z - pos.z)))
+					if distance <= 1 then
 						if inv and inv:room_for_item("main", ItemStack(object:get_luaentity().itemstring)) then
-							local pos1 = pos
-							pos1.y = pos1.y+0.2
-							local pos2 = object:getpos()
-							local vec = {x=pos1.x-pos2.x, y=pos1.y-pos2.y, z=pos1.z-pos2.z}
-							vec.x = vec.x*3
-							vec.y = vec.y*3
-							vec.z = vec.z*3
-							object:setvelocity(vec)
-							object:get_luaentity().physical_state = false
-							object:get_luaentity().object:set_properties({
-								physical = false
-							})
-							
-							minetest.after(1, function(args)
-								local lua = object:get_luaentity()
-								if object == nil or lua == nil or lua.itemstring == nil then
-									return
-								end
-								if inv:room_for_item("main", ItemStack(object:get_luaentity().itemstring)) then
-									inv:add_item("main", ItemStack(object:get_luaentity().itemstring))
-									if object:get_luaentity().itemstring ~= "" then
-										minetest.sound_play("item_drop_pickup", {
-											to_player = player:get_player_name(),
-											gain = 0.4,
+							inv:add_item("main", ItemStack(object:get_luaentity().itemstring))
+							if object:get_luaentity().itemstring ~= "" then
+								minetest.sound_play("item_drop_pickup", {
+									to_player = player:get_player_name(),
+									gain = 0.4,
+								})
+							end
+							object:get_luaentity().itemstring = ""
+							object:remove()
+						end
+					else
+						if object:get_luaentity().collect then
+							if inv and inv:room_for_item("main", ItemStack(object:get_luaentity().itemstring)) then
+								local pos1 = pos
+								pos1.y = pos1.y+0.2
+								local vec = {x=pos1.x-pos2.x, y=pos1.y-pos2.y, z=pos1.z-pos2.z}
+								vec.x = vec.x*3
+								vec.y = vec.y*3
+								vec.z = vec.z*3
+								object:setvelocity(vec)
+								object:get_luaentity().physical_state = false
+								object:get_luaentity().object:set_properties({
+									physical = false
+								})
+								
+								minetest.after(1, function(args)
+									local lua = object:get_luaentity()
+									if object == nil or lua == nil or lua.itemstring == nil then
+										return
+									end
+									if inv:room_for_item("main", ItemStack(object:get_luaentity().itemstring)) then
+										inv:add_item("main", ItemStack(object:get_luaentity().itemstring))
+										if object:get_luaentity().itemstring ~= "" then
+											minetest.sound_play("item_drop_pickup", {
+												to_player = player:get_player_name(),
+												gain = 0.4,
+											})
+										end
+										object:get_luaentity().itemstring = ""
+										object:remove()
+									else
+										object:setvelocity({x=0,y=0,z=0})
+										object:get_luaentity().physical_state = true
+										object:get_luaentity().object:set_properties({
+											physical = true
 										})
 									end
-									object:get_luaentity().itemstring = ""
-									object:remove()
-								else
-									object:setvelocity({x=0,y=0,z=0})
-									object:get_luaentity().physical_state = true
-									object:get_luaentity().object:set_properties({
-										physical = true
-									})
-								end
-							end, {player, object})
-							
+								end, {player, object})
+								
+							end
 						end
 					end
 				end
